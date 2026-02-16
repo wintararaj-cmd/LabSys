deee4e0ba097988f88fd524cfceee2c9d3eca0a5
wintararaj-cmd <win.tararaj@gmail.com>
2026-02-16 23:38:21 +0530

    Deployment: Make DB SSL configurable via DB_SSL env var

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 // Create a connection pool
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
     max: 20, // Maximum number of clients in the pool
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
